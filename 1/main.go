6fa550f8f4f6f3f1f3e10bb94e828d7a90788fc7
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-09-21 20:34:36 +0700

    feat: set up ginx

---
+++ 1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/ping/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		price := ctx.Query("price")
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+			"id":      id,
+			"price":   price,
+		})
+	})
+	// Router group
+
+	
+	router.Run(":8080")
+}
