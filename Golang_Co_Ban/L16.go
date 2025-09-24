d97d3494e8b68fb95b58ebb86fb2941ed404f0c7
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-09-24 20:22:39 +0700

    fix: missing ex3

--- Golang_Co_Ban/L16.go
+++ Golang_Co_Ban/L16.go
@@ -15,10 +15,58 @@ func main() {
 	// 	fmt.Print(i, ", ")
 	// }
 	//B2
-	for i := 1; i <= 100; i += 2 {
-		for j := 1; j <= 3; j++ {
+	// maxx := 100
+	// maxxOdd := 1
+	// count := 0
+	// if maxx%2 == 1 {
+	// 	maxxOdd = maxx
+	// } else {
+	// 	maxxOdd = maxx - 1
+	// }
+	// fmt.Println("maxxOdd:", maxxOdd)
+	// for i := 1; i <= maxx; i++ {
+	// 	if i%2 == 1 {
+	// 		count++
+	// 		if i == maxxOdd {
+	// 			fmt.Print(i, ".")
+	// 			break
+	// 		}
+	// 		if count == 3 {
+	// 			fmt.Print(i, "")
+	// 		} else {
+	// 			fmt.Print(i, ", ")
+	// 		}
+	// 	}
+	// 	if count == 3 {
+	// 		count = 0
+	// 		fmt.Println()
+	// 	}
+	// }
+	//Ex3
+	var start int
+	var end int
+	fmt.Print("Enter start: ")
+	_, err := fmt.Scanf("%d", &start)
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	fmt.Print("Enter end: ")
+	_, err = fmt.Scanf("%d", &end)
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	if (start < 0 || end < 0) || (start > end) || end > 10 {
+		fmt.Println("Invalid input")
+		return
+	}
+	for i := start; i <= end; i++ {
+		if i%2 == 1 {
 			fmt.Print(i, ", ")
 		}
-		fmt.Println()
 	}
+
 }
