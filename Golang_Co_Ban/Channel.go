3678ec7c4d28e1b2c559795211eaade3174fff50
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2026-02-23 22:31:54 +0700

    feat: add select

--- Golang_Co_Ban/Channel.go
+++ Golang_Co_Ban/Channel.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
 
-func main() {
+func task(id int, ch chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println("Task", id, "is running")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Task", id, "is ending")
+	ch <- "Task " + strconv.Itoa(id) + " is done"
+}
+func Channel() {
 	// Channel là 1 kênh để truyền dữ liệu giữa các goroutine
 	// Các loại Channel
 	// Unbuffered Channel: Chỉ chứa được 1 gtri tại 1 thời điểm
@@ -12,11 +25,18 @@ func main() {
 	// Buffered Channel: Có thể chứa nhiều gtri
 	// Gửi (ch <- 1) sẽ bị chặn khi buffer đầy
 	// Nhận (<-ch) sẽ bị chặn khi buffer rỗng
+	var wg sync.WaitGroup
+	cha := make(chan string, 3)
 
-	ch := make(chan int)
-	go func() {
-		ch <- 1
-	}()
-	fmt.Println(<-ch)
+	for i := range 3 {
+		wg.Add(1)
+		go task(i, cha, &wg)
+	}
 
+	wg.Wait()
+	close(cha)
+
+	for val := range cha {
+		fmt.Println(val)
+	}
 }
---
+++ Golang_Co_Ban/Select.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Select là 1 cơ chế để chọn nhiều channel để nhận dữ liệu
+	// Các loại Select
+	// Unbuffered Channel: Chỉ chứa được 1 gtri tại 1 thời điểm
+	// Buffered Channel: Chứa được nhiều gtri tại 1 thời điểm
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "two"
+	}()
+	for i := range 2 {
+		fmt.Println(i)
+		select {
+		case msg1 := <-ch1:
+			fmt.Println("received", msg1)
+		case msg2 := <-ch2:
+			fmt.Println("received", msg2)
+		}
+	}
+
+}
